168cc2d161827c2fbf04d21562ccd1f65257ff57
ShaderGuy <jshubert1994@gmail.com>
2020-12-12 15:50:02 -0800

    Added macOS build action and changed build.sh (#4)
    * Added macOS build action and changed build.sh to dump build artifacts into a build directory
    
    * Updated the if statement check to work properly with build folder
    
    Co-authored-by: Joshua Shubert <josh@avendahealth.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-if [ ! -f ./Makefile -o ./Makefile -nt se.pro ]; then
-	qmake se.pro -o Makefile
+if [ ! -f ../build/Makefile -o ../build/Makefile -nt se.pro ]; then
+  mkdir -p build
+  cd build && qmake ../se.pro -o Makefile
 fi
 
 make $@
